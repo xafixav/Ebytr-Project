cf7532e51525d2c43fef93d775d2fa71e8bd38d3
xafixav <xafiworld@gmail.com>
2022-05-17 22:27:02 -0300

    :white_check_mark: create an reusable Input component

---
+++ app/frontend/src/components/OnChangeInput.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default class OnChangeInput extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const { name, onChange, type, dataTestId } = this.props;
+    return (
+      <div>
+        <input name={name} onChange={onChange()} type={type} data-testid={dataTestId} />
+      </div>
+    );
+  }
+}
+
+OnChangeInput.propTypes = {
+  onChange: PropTypes.func.isRequired,
+  name: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  dataTestId: PropTypes.string.isRequired,
+};
