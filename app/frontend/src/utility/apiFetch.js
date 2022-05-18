00be4ae2a7001dee30a7077727a4cab1662ac577
xafixav <xafiworld@gmail.com>
2022-05-18 00:41:42 -0300

    :ambulance: fix loginFetch and add tasksFetch

--- app/frontend/src/utility/apiFetch.js
+++ app/frontend/src/utility/apiFetch.js
@@ -20,11 +20,42 @@ export default class ApiFetch {
     const response = await fetch(
       `https://localhost:${process.env.REACT_APP_BACKEND_PORT || '3001'}/login`,
       fetchOption,
-    ).catch((error) => {
-      if (error) {
-        return 'Failed to fetch';
-      }
-    });
+    )
+      .then((resp) => {
+        return JSON.parse(resp);
+      })
+      .then((parsedResponse) => {
+        return parsedResponse;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+    return response;
+  };
+
+  tasksFetch = async () => {
+    const token = localStorage.getItem('userToken');
+    const fetchOption = {
+      method: 'GET',
+      headers: {
+        'Content-type': 'application/json',
+        Authorization: token,
+      },
+    };
+
+    const response = await fetch(
+      `https://localhost:${process.env.REACT_APP_BACKEND_PORT || '3001'}/tasks`,
+      fetchOption,
+    )
+      .then((resp) => {
+        return JSON.parse(resp);
+      })
+      .catch((error) => {
+        if (error) {
+          return 'Failed to fetch';
+        }
+      });
 
     return response !== 'Failed to fetch';
   };
