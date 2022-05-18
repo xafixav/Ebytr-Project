99e9e850559fda2b27e198b6560c23b1ed71b0a5
xafixav <xafiworld@gmail.com>
2022-05-17 22:30:42 -0300

    :sparkles: add user and password Input into Login

--- app/frontend/src/pages/Login.jsx
+++ app/frontend/src/pages/Login.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import OnChangeInput from '../components/OnChangeInput';
 
 export default class Login extends React.Component {
   constructor(props) {
@@ -16,6 +17,21 @@ export default class Login extends React.Component {
   };
 
   render() {
-    return <div>hello world</div>;
+    return (
+      <div>
+        <OnChangeInput
+          onChange={() => this.handleChange}
+          name="user"
+          type="text"
+          dataTestId="userLoginInput"
+        />
+        <OnChangeInput
+          onChange={() => this.handleChange}
+          name="password"
+          type="password"
+          dataTestId="passwordLoginInput"
+        />
+      </div>
+    );
   }
 }
