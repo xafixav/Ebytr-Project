4e1a6967c50f638138dcbed2a73f50f8101ed9b8
xafixav <xafiworld@gmail.com>
2022-05-18 00:18:24 -0300

    :white_check_mark: add Login Button and its logic

--- app/frontend/src/pages/Login.jsx
+++ app/frontend/src/pages/Login.jsx
@@ -1,19 +1,59 @@
 import React from 'react';
 import OnChangeInput from '../components/OnChangeInput';
+import OnClickButton from '../components/OnClickButton';
+import ApiFetch from '../utility/apiFetch';
+import PropTypes from 'prop-types';
 
 export default class Login extends React.Component {
   constructor(props) {
     super(props);
 
+    this.api = new ApiFetch();
+
     this.state = {
       user: '',
       password: '',
+      noEmptyField: false,
     };
   }
 
-  handleChange = (component) => {
+  updateState = (component) => {
     const { name, value } = component.target;
     this.setState({ [`${name}`]: value });
+  };
+
+  handleChange = (component) => {
+    const { user, password, noEmptyField } = this.state;
+    this.updateState(component);
+    if (user && password) {
+      return this.setState({ noEmptyField: true });
+    }
+    if ((!user || !password) && noEmptyField === true) {
+      return this.setState({ noEmptyField: false });
+    }
+  };
+
+  handleClick = async () => {
+    const { user, password } = this.state;
+    const { loginFetch } = this.api;
+    const { history } = this.props;
+    const token = await loginFetch({ user, password });
+    if (typeof token === 'string') {
+      localStorage.setItem('userToken', token);
+      history.push('/tasks');
+    }
+  };
+
+  renderLoginButton = () => {
+    const { noEmptyField } = this.state;
+    return (
+      <OnClickButton
+        onClick={() => this.handleClick}
+        noEmptyField={!noEmptyField}
+        innerText="Login"
+        dataTestId="LoginButton"
+      />
+    );
   };
 
   render() {
@@ -31,7 +71,12 @@ export default class Login extends React.Component {
           type="password"
           dataTestId="passwordLoginInput"
         />
+        {this.renderLoginButton()}
       </div>
     );
   }
 }
+
+Login.propTypes = {
+  history: PropTypes.object.isRequired,
+};
