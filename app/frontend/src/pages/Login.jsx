18125592027324adc7658a485da838a785a2a1e0
xafixav <xafiworld@gmail.com>
2022-05-19 20:15:22 -0300

    :sparkles: reduce the size of apiMessage error and add a h2 for usuario and password

--- app/frontend/src/pages/Login.jsx
+++ app/frontend/src/pages/Login.jsx
@@ -64,21 +64,25 @@ export default class Login extends React.Component {
     const { apiMessage, user, password } = this.state;
     return (
       <div>
-        <h1>{apiMessage}</h1>
-        <OnChangeInput
-          onChange={() => this.handleChange}
-          name="user"
-          type="text"
-          dataTestId="userLoginInput"
-          value={user}
-        />
-        <OnChangeInput
-          onChange={() => this.handleChange}
-          name="password"
-          type="password"
-          value={password}
-          dataTestId="passwordLoginInput"
-        />
+        <h2>{apiMessage}</h2>
+        <div>
+          <h2>Usuario</h2>
+          <OnChangeInput
+            onChange={() => this.handleChange}
+            name="user"
+            type="text"
+            dataTestId="userLoginInput"
+            value={user}
+          />
+          <h2>Password</h2>
+          <OnChangeInput
+            onChange={() => this.handleChange}
+            name="password"
+            type="password"
+            value={password}
+            dataTestId="passwordLoginInput"
+          />
+        </div>
         {this.renderLoginButton()}
       </div>
     );
