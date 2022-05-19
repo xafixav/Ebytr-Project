d8983e2da0f8086920c8713ff9e673e0ea48a23f
xafixav <xafiworld@gmail.com>
2022-05-19 07:45:25 -0300

    :white_check_mark: add updateTask into Tasks Controller

--- app/backend/src/controller/Tasks.ts
+++ app/backend/src/controller/Tasks.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
 import { StatusCodes } from 'http-status-codes';
+import IUpdateTask from '../interfaces/IUpdateTask';
 import TasksService from '../service/Tasks';
 
 export default class MatchesController {
@@ -34,6 +35,27 @@ export default class MatchesController {
       const matchesFiltered = await this.TasksService
         .createTask({ task, status }, token);
       return res.status(StatusCodes.CREATED).json(matchesFiltered);
+    } catch (e) {
+      next(e);
+    }
+  };
+
+  public updateTask = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { id, task, status: taskStatus } = req.body;
+      const { authorization } = req.headers;
+      if (!authorization) {
+        return res.status(StatusCodes.BAD_REQUEST).json({ message: 'Token not exist' });
+      }
+
+      const updateRequest: IUpdateTask = {
+        id: +id,
+        task,
+        status: taskStatus,
+        token: authorization,
+      };
+      const matchUpdated = await this.TasksService.updateTask(updateRequest);
+      return res.status(StatusCodes.OK).json(matchUpdated);
     } catch (e) {
       next(e);
     }
