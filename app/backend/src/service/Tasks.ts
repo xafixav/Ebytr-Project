f53436bc900be9df0d115066e205d1612f1458b0
xafixav <xafiworld@gmail.com>
2022-05-19 07:39:23 -0300

    :sparkless: add findAll into Tasks Service

---
+++ app/backend/src/service/Tasks.ts
@@ -0,0 +1,33 @@
+import { StatusCodes } from 'http-status-codes';
+import ErrorExtension from '../utility/ErrorExtension';
+import Tasks from '../database/models/Tasks';
+import LoginService from './Login';
+
+export default class TasksService {
+  private static noTasksFound = 'No tasks has been found';
+
+  private loginService: LoginService;
+
+  constructor() {
+    this.loginService = new LoginService();
+  }
+
+  public findAll = async (token: string) => {
+    const User = await this.loginService.getUserByToken(token);
+
+    if (!User?.id) {
+      throw new ErrorExtension({ status: StatusCodes.UNAUTHORIZED,
+        message: 'It is not possible to edit a task in which you are not the owner' });
+    }
+
+    const allTasks = await Tasks.findAll({ where: { userId: User.id }});
+
+    if (!allTasks) {
+      throw new ErrorExtension({ status: StatusCodes.NOT_FOUND,
+        message: TasksService.noTasksFound });
+    }
+
+    return allTasks;
+  };
+
+}
