708474259dac44f1cc88b19befdeab2679cabd10
xafixav <xafiworld@gmail.com>
2022-05-19 07:00:29 -0300

    :card_file_box: add Tasks Model

---
+++ app/backend/src/database/models/Tasks.ts
@@ -0,0 +1,45 @@
+import { DataTypes, Model } from 'sequelize';
+import db from '.';
+import Users from './Users';
+
+class Tasks extends Model {
+  public id: number;
+
+  public tasks: string;
+
+  public userId: number;
+
+  public status: string;
+}
+
+Tasks.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    autoIncrement: true,
+    unique: true,
+    primaryKey: true,
+  },
+  tasks: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: false,
+  },
+  userId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    unique: false,
+  },
+  status: {
+    type: DataTypes.STRING,
+  },
+}, {
+  underscored: false,
+  sequelize: db,
+  tableName: 'tasks',
+  timestamps: false,
+});
+
+Tasks.belongsTo(Users, { foreignKey: 'userId', as: 'user' });
+
+export default Tasks;
