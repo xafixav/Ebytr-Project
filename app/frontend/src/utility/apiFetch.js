9ac857b9aa9649dd067aa358a506becfe0afa487
xafixav <xafiworld@gmail.com>
2022-05-20 15:02:01 -0300

    :sparkles: add GET, POST, PUT tasks

--- app/frontend/src/utility/apiFetch.js
+++ app/frontend/src/utility/apiFetch.js
@@ -37,11 +37,62 @@ export default class ApiFetch {
     };
 
     const responseGetTasks = await fetch(
-      `https://localhost:${process.env.REACT_APP_BACKEND_PORT || '3001'}/tasks`,
+      `http://localhost:${process.env.REACT_APP_BACKEND_PORT || '3001'}/tasks`,
       fetchOption,
     );
     const parseResponse = await responseGetTasks.json();
-    console.log(parseResponse);
+
+    return parseResponse;
+  };
+
+  taskPost = async (payload) => {
+    const token = localStorage.getItem('userToken');
+    const { task, status } = payload;
+    if (!task) {
+      return;
+    }
+    const fetchOption = {
+      method: 'POST',
+      body: JSON.stringify({
+        task,
+        status,
+      }),
+      headers: {
+        'Content-type': 'application/json',
+        Authorization: token,
+      },
+    };
+
+    const responseLogin = await fetch(
+      `http://localhost:${process.env.REACT_APP_BACKEND_PORT || '3001'}/tasks`,
+      fetchOption,
+    );
+    const parseResponse = await responseLogin.json();
+
+    return parseResponse;
+  };
+
+  taskPut = async (payload) => {
+    const token = localStorage.getItem('userToken');
+    const { task, status, id } = payload;
+    const fetchOption = {
+      method: 'PUT',
+      body: JSON.stringify({
+        task,
+        status,
+        id,
+      }),
+      headers: {
+        'Content-type': 'application/json',
+        Authorization: token,
+      },
+    };
+
+    const responseLogin = await fetch(
+      `http://localhost:${process.env.REACT_APP_BACKEND_PORT || '3001'}/tasks`,
+      fetchOption,
+    );
+    const parseResponse = await responseLogin.json();
 
     return parseResponse;
   };
